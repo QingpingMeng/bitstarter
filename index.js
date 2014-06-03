d792c5818723a2abcbbc7ebf82d5db747c9f2d03
QingpingMeng <missing1989@gmail.com>
2014-06-03 04:04:58 +0000

    Added

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	var fs=require('fs');
+	fs.readFile('./index.html',function(err,data){
+		if(err) throw err;
+		var buf=new Buffer(data);
+		response.send(buf.toString('utf-8'));
+	})
 })
 
 app.listen(app.get('port'), function() {
